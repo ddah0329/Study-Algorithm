ce212b012300f56b4b4b36eee5163b15dbe037e2
ddah0329 <ddah0329m@gmail.com>
2025-01-21 21:37:42 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 67.74 ms, Memory: 27.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
@@ -0,0 +1,10 @@
+from collections import deque
+
+def solution(arr):
+    queue = deque()
+    
+    for num in arr:
+        if not queue or queue[-1] != num:
+            queue.append(num)  
+    
+    return list(queue)
